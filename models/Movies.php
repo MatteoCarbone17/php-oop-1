794b3ef2d56e35fdc31e615da80eaae231690b7f
MatteoCarbone17 <matteocarbone17@live.com>
2023-02-03 15:30:48 +0100

    create a constructor

--- models/Movies.php
+++ models/Movies.php
@@ -1,20 +1,35 @@
 <?php 
 
 class Movies{
-    // campi(caratteristiche tipiche)
-    public $title;
+   
+    public $title; // campi(caratteristiche/attributi(variabili)  tipiche)
     public $type;
     public $duration;
     public $plot;
 
+    //costruttore 
+    public  function __construct( $_title, $_type,$_duration, $_plot)
+    {
+        //inizializza una istanza di tipo Movie 
+        $this->title= $_title; //  $this->title si riferisce a public $title; |||| mentre $_title si riferisce a  __construct( $_title) 
+        $this->type= $_type;
+        $this->duration= $_duration;
+        $this->plot= $_plot;
+        
+    }
+ 
     //methods(capacita condivise)
 
     public function getMovie(){
-        
+
     }
 
 
 }
 
+$movieTheMist = new Movies('The Mist','horror' ,'120 minuti','roba strana dall\'inferno, finale trumatico' );
+$movieTitanic = new Movies('Titanic','romatico' ,'dura troppo','Una nave inaffondabile che poi affonda' );
+
+var_dump($movieTheMist,$movieTitanic);
 
 ?>
