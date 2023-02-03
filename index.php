1759cbad49ab1e6a20aaf10dbccf5cd4b798928f
MatteoCarbone17 <matteocarbone17@live.com>
2023-02-03 16:39:02 +0100

    add methods

--- index.php
+++ index.php
@@ -1,28 +1,47 @@
 <?php
 require_once __DIR__ . '/models/Cast.php';
 require_once __DIR__ . '/models/Movie.php';
-$movieTheMist = new Movie('The Mist','horror' ,'120 minuti','roba strana dall\'inferno, finale trumatico', new Cast ('Thomas Jane', 'Laurie Holden', 'devil') );
-$movieTitanic = new Movie('Titanic','romatico' ,'dura troppo','Una nave inaffondabile che poi affonda',new Cast ('Leonardo DiCaprio ', 'Kate Winslet', 'iceberg') );
+$movieTheMist = new Movie('The Mist', 'horror', '120 minuti', 'roba strana dall\'inferno, finale trumatico', 8, new Cast('Thomas Jane', 'Laurie Holden', 'devil'));
+$movieTitanic = new Movie('Titanic', 'romatico', 'dura troppo', 'Una nave inaffondabile che poi affonda', 5, new Cast('Leonardo DiCaprio ', 'Kate Winslet', 'iceberg'));
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP OOP</title>
 </head>
+
 <body>
     <main>
         <h1>
             OOP PHP MOVIES
         </h1>
         <pre>
-            <?php        
-            var_dump($movieTheMist,$movieTitanic);
-            ?>
+            <?php
+               var_dump($movieTheMist);
+             ?>
         </pre>
+        <p>
+            <?php
+            var_dump($movieTheMist->getRating());
+            ?>
+
+        </p>
+        <pre>
+            <?php
+                 var_dump($movieTitanic);
+             ?>
+        </pre>
+        <p>
+        <?php
+            var_dump($movieTitanic->getRating());
+            ?>
+        </p>
     </main>
 </body>
+
 </html>
--- models/Movie.php
+++ models/Movie.php
@@ -6,23 +6,32 @@ class Movie{
     public $type;
     public $duration;
     public $plot;
+    public $rating;
     public $cast;
 
     //costruttore 
-    public  function __construct( $_title, $_type,$_duration, $_plot, Cast $_cast )
+    public  function __construct( $_title, $_type,$_duration, $_plot,$_rating, Cast $_cast )
     {
         //inizializza una istanza di tipo Movie 
         $this->title= $_title; //  $this->title si riferisce a public $title; |||| mentre $_title si riferisce a  __construct( $_title) 
         $this->type= $_type;
         $this->duration= $_duration;
         $this->plot= $_plot;
+        $this->rating= $_rating;
         $this->cast= $_cast;
         
     }
  
     //methods(capacita condivise)
 
-    public function getMovie(){
+    public function getRating(){
+
+        if ($this->rating >= 6){
+          return 'Valutazione film: '. $this->rating . '/10 ' . 'sufficienza raggiunta' ;
+        }else{
+            return 'Valutazione film: '. $this->rating . '/10 ' . 'sufficienza non raggiunta' ; 
+        };
+
 
     }
 
