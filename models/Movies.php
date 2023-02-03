99e60c7ebb3455f482dc5b87cba2eb0c08bd6100
MatteoCarbone17 <matteocarbone17@live.com>
2023-02-03 14:55:54 +0100

    create file

---
+++ models/Movies.php
@@ -0,0 +1,20 @@
+<?php 
+
+class Movies{
+    // campi(caratteristiche tipiche)
+    public $title;
+    public $type;
+    public $duration;
+    public $plot;
+
+    //methods(capacita condivise)
+
+    public function getMovie(){
+        
+    }
+
+
+}
+
+
+?>
